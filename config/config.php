fafdcee5b59fc22afdcb7743603382883cfad39a
William Hunt <will@huntdesignco.com>
2020-10-04 21:05:06 -0400

    updated bootstrap and added less

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 // Some basic configuration settings
-define( 'SITE_DOMAIN', 'mysite.com' );
+define( 'SITE_DOMAIN', 'migration-tool.local' );
 define( 'SITE_FOLDER', '');
 define( 'USE_SSL', true);
 
